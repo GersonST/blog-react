1e1bb084117dea0229c1492519d3d0f49e2146f6
GersonST <gersonsevero90@gmail.com>
2021-06-13 03:43:06 -0300

    Painel

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import app from 'firebase/app';
 import 'firebase/database';
 import 'firebase/auth';
 
-
+//Configurações do seu projeto
 let firebaseConfig = {
     apiKey: "AIzaSyAWA8aq_WcubafK3L6GkGwBslz4Y0nMZ8E",
     authDomain: "blog-3cde8.firebaseapp.com",
@@ -18,11 +18,18 @@ class Firebase{
     // Initialize Firebase
     app.initializeApp(firebaseConfig);
 
+    //Referenciando a database para acessar em outros locais
     this.app = app.database();
+
+    
   }
 
   login(email, password){
     return app.auth().signInWithEmailAndPassword(email,password)
+  }
+
+  logout(){
+    return app.auth().signOut();
   }
 
   async register(nome,email, password){
@@ -42,6 +49,18 @@ class Firebase{
 
   getCurrent(){
     return app.auth().currentUser && app.auth().currentUser.email;
+  }
+
+  async getUserName(callback){
+
+    if(!app.auth().currentUser){
+      return null;
+    }
+
+    const uid = app.auth().currentUser.uid;
+
+    await app.database().ref('usuarios').child(uid).once('value').then(callback);
+
   }
 
 }
